f7b2daf7721aef034e4e87c6a99b86396c210207
lorenzo venneri <lorenzo.venneri@smc.it>
2023-02-07 09:36:29 +0100

    Fix

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -24,20 +24,6 @@ export function NavBar() {
             <Logo />
             <Button color="primary">test</Button>
             <Button color="primary">test</Button>
-
-            {/* <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              ciao
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button> */}
           </Toolbar>
         </AppBar>
       </Box>
